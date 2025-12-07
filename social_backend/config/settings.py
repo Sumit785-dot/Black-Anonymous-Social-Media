d3881090cd196649cb720b36e6a854f6cfd0e79b
sumit mewada <sumitmewada736@gmail.com>
2025-12-07 13:24:15 +0530

    Fix Dockerfile and render.yaml for deployment

--- social_backend/config/settings.py
+++ social_backend/config/settings.py
@@ -78,11 +78,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+import dj_database_url
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': config(
+        'DATABASE_URL',
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        cast=dj_database_url.parse
+    )
 }
 
 # Redis Cache Configuration - Performance Optimization
